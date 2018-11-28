fef53b00d5a5f4f43550a084b4f7baa0e10a98d0
Xing Liang <40831339+XingLiangLondon@users.noreply.github.com>
2018-11-28 11:48:14 +0000

    Update first_draft.py

--- first_draft.py
+++ first_draft.py
@@ -8,24 +8,29 @@ import time
 cap = cv2.VideoCapture(1)
 
 # Initialize video input
-cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Pronated Wrist/WATCH2.mp4")
+#cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Pronated Wrist/WATCH2.mp4")
+#cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Stomach/FARM.mp4")
+#cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Below Waist/LAP.mp4")
 
-# Set Tracking Delay in terms of number of frames to make KNN background subtraction work
-DELAY= 10
+# Set Tracking Delay (i.e. delay in number of frames) to wait for KNN background subtraction work (Camera: 25; Video: 5)
+DELAY= 5
 
-# Set countour radius to denoise
-RADIUS = 30
+# Set countour radius to denoise, only contours which is big enough will be tracked 
+RADIUS = 25
 
-# Get video details
+# Set frame count for tracking trails reset, if there is no hands being detected
+frame_count = 0
+
+# Get video/camera input details
 """
-#lengthVideo = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+lengthVideo = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 widthVideo  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 heightVideo = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# For a video input with stream header (need nb_frames field) 
-fpsVideo    = int(cap.get(cv2.CAP_PROP_FPS)) 
+# fps for video input with a stream header (need nb_frames field) 
+fpsVideo = int(cap.get(cv2.CAP_PROP_FPS)) 
 
-# For a camera input without stream header 
+# fps for camera input without a stream header 
 # Number of frames to capture
 num_frames = 120;         
 print ("Capturing {0} frames".format(num_frames))
@@ -48,11 +53,11 @@ print ("Estimated frames per second : {0}".format(fpsCamera))
 #lower_thresh = np.array([0, 50, 0])
 #upper_thresh = np.array([120, 150, 255])
 
-# define range of skin color in HSV
+# define range of skin color in HSV (works better with brown skin)
 lower_thresh = np.array([0, 48, 80], dtype = "uint8")
 upper_thresh = np.array([20, 255, 255], dtype = "uint8")
 
-# Create empty points array for trajectories tracking 
+# Create empty points array for hand trajectories tracking 
 points_left = []
 points_right = []
 
@@ -106,7 +111,7 @@ term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 """
 
 
-# loop video capture until break statement is exectured
+# Loop video capture until break statement is exectured
 while cap.isOpened(): 
     
     # Read webcam/video image
@@ -117,9 +122,9 @@ while cap.isOpened():
 
         # Get default camera/video window size
         Height, Width = frame.shape[:2]
-        frame_count = 0
+        
        
-        # Convert image from RBG/BGR to HSV so we easily filter
+        # Convert image from RBG/BGR to HSV 
         hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         
         # Face Detection Using HAAR CASCADE 
@@ -134,34 +139,34 @@ while cap.isOpened():
         # Use inRange to capture only the values between lower & upper_thresh for skin detection
         mask = cv2.inRange(hsv_img, lower_thresh, upper_thresh) 
         
-        # Adding morphology effects to get rid of noises
+        # Adding morphology effects to denoise
         kernel_morphology =np.ones((5, 5), np.uint8)
         mask = cv2.erode(mask, kernel_morphology, iterations=1)
         #mask=cv2.morphologyEx(mask,cv2.MORPH_OPEN, kernel_morphology)
         mask=cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel_morphology)
         mask = cv2.dilate(mask, kernel_morphology, iterations=1)
-        
+        cv2.imshow('Skin colour Mask', mask)
+
         # Perform Bitwise AND on mask and original frame
-        # rest1 is result of morphology effects
+        # rest1 is the results after applying morphology effects
         rest1 = cv2.bitwise_and(frame, frame, mask= mask)
         
-        # Apply KKN background subtraction to refine skin filtering result, i.e. to further remove static skin coulor related background (including face is it's not move) 
+        # Apply KKN background subtraction to refine skin filtering result, i.e. to further remove static skin coulor related background (face will be fading out, if it does not move) 
         fgmask = fgbg.apply(rest1)
         fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel_bgsub)
         cv2.imshow('Background subtraction Mask',fgmask) 
 
         # Perform Bitwise AND on fgmask and rest1 frame
-        # rest2 is result of background subtraction and morphology effects
+        # rest2 is results after applying background subtraction and morphology effects
         rest2 = cv2.bitwise_and(rest1, rest1, mask= fgmask)
 
-    
         # Find contours 
         # cv2.RETR_EXTERNAL finds external contours only; cv2.CHAIN_APPROX_SIMPLE only provides start and end points of bounding contours, thus resulting in much more efficent storage of contour information.
         _, contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         print ("Number of contours1 found = ", len(contours))
         #print(type(contours))   #The variable 'contours' are stored as a numpy array of (x,y) points that form the contour
         
-     
+        
         # Find Canny Edges (not helpful!)
         """
         edged = cv2.Canny(rest2, 30, 240)
@@ -181,8 +186,7 @@ while cap.isOpened():
         #cv2.imshow('All Contours filtered by skin color and background subtraction', rest2)
         
         #cv2.imshow('Original', frame)  
-        cv2.imshow('Skin colour Mask', mask)
-        
+
 
         # CAMshift
         """
@@ -202,67 +206,64 @@ while cap.isOpened():
         
         cv2.imshow('Camshift Tracking', img2)
         """
-        
-        
+
         # Create empty centre array to store centroid center of mass
         center = int(Height*2/3), int(Width*2/3)
 
+        # When both hands are detected 
         if len(contours) >=2:
         
             # Get the largest two contours and its center (i.e. two hands)
             sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)[:2]
             
-            # Sort by left to right using our x_cord_contour function (i.e. hands left to right)
+            # Sort by left to right using our x_cord_contour function (i.e. hands tracking from left to right)
             contours_left_to_right = sorted(sorted_contours, key = x_cord_contour, reverse = False)
 
-            
             # Iterate over two contours and draw one at a time
             for (i,c) in enumerate(contours_left_to_right):
                 
-                # Draw Convex Hull Contour
-                
+                # Draw Convex Hull Contour   
                 hull=cv2.convexHull(c)
                 cv2.drawContours(rest2, [hull], -1, (0,0,255), 3)
-      
-                 
+                      
                 # Draw Normal Contour
                 cv2.drawContours(rest2, [c], -1, (255,0,0), 3)
                 
                 # Show hands Contour
                 cv2.imshow('Contours by area', rest2)
 
+
                 # Tracking Left hand   
-                if i==0:
+                if i == 0:
                     (x, y), radius = cv2.minEnclosingCircle(c)
                     M = cv2.moments(c)
                     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
                     # Draw cirlce and leave the last center creating a trail
                     cv2.circle(frame, (int(x), int(y)), int(radius),(0, 0, 255), 2)
                     
-                    # loop over the set of tracked points
+                    # Only contours with radius > RADIUS are tracked (de-noise)
                     if  radius > RADIUS:
-
-                        # Only contours with radius >RADIUS are tracked (de-noise)
                         points_left.append(center)
-                        # loop over to draw tracing lines (draw tracing line starts with frames delay- approximately 1 seconds delay to make KNN background subtraction work)
+                        # loop over the set of tracked points to draw tracking lines (starts with frames delay- approximately 1 seconds delay to make KNN background subtraction work)
                         for l in range(DELAY, len(points_left)):
                             try:
                                 cv2.line(frame, points_left[l - 1], points_left[l], (0, 0, 255), 2)
                             except:
                                 pass
-                        # Make frame count zero
                         frame_count = 0
                     else:
-                        # Count frames 
+                         # If there is no hand detected, count frames 
                         frame_count += 1
-                        
-                        # when frame_count reaches 20, let's clear our trail
-                        if frame_count == 20:
+                        print("frame_count",frame_count)
+                        # when frame_count reaches 20, clear our trails
+                        if frame_count == 5:
                             points_left = []
                             points_right = [] 
                             frame_count = 0
+       
+                            
                 # Tracking Right hand  
-                elif i==1:    
+                elif i == 1:    
                     (x, y), radius = cv2.minEnclosingCircle(c)
                     M = cv2.moments(c)
                     center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -277,26 +278,29 @@ while cap.isOpened():
                                 cv2.line(frame, points_right[l - 1], points_right[l], (0, 255, 0), 2)
                             except:
                                 pass
-                        frame_count = 0
+                        frame_count = 0   
+
                     else:
-                       # Count frames 
+                        # If there is no hand detected, count frames 
                         frame_count += 1
-                         # when frame_count reaches 20 let's clear our trail
-                        if frame_count == 20:
-                           points_left = []
-                           points_right = [] 
-                           frame_count = 0
+                        # when frame_count reaches 50, clear our trails
+                        if frame_count == 50:
+                            points_left = []
+                            points_right = [] 
+                            frame_count = 0
+
+
                 else:
                      pass
 
+
         else:
-           pass
+          pass
 
 
         # Display our object tracker
         frame = cv2.flip(frame, 1)
         cv2.imshow("Object Tracker", frame)
-
 
         if cv2.waitKey(1) == 13: #13 is the Enter Key
             break
