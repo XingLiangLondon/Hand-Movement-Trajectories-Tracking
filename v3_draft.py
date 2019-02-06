0a6ca1e3afbfaea4afb2bbcdb1d0245fcbbd546b
Xing Liang <40831339+XingLiangLondon@users.noreply.github.com>
2019-02-06 14:13:40 +0000

    Update v3_draft.py

--- v3_draft.py
+++ v3_draft.py
@@ -1,38 +1,35 @@
 import cv2
 import numpy as np
-import time
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from datetime import datetime
-
-#from matplotlib import dates
-#import importlib
-#importlib.import_module('mpl_toolkits.mplot3d').__path__
-
+from mpl_toolkits.mplot3d import Axes3D
+#import time
 
 # Initialize webcam input
-#cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
 # Initialize video input
-cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Pronated Wrist/WATCH2.mp4")
 #cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Stomach/FARM.mp4")
 #cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Below Waist/LAP.mp4")
 #cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Restricted/L2n.mov")
+#cap = cv2.VideoCapture("C:/Users/liangx/Documents/Dunhill Project Data/Single Sign/Pronated Wrist/WATCH2.mp4")
 
+# Set different colour converstion models {1 : HSV,2 : YCrCb,3 : LAB, 4 : XYZ,}
+COLOUR_MODEL = 1;            
+           
 # Set Tracking Delay (i.e. delay in number of frames) to wait for KNN background subtraction work (Camera: 30; Video: 5)
-DELAY= 5
+DELAY= 10
 
 # Set countour radius to denoise, only contours bigger enough are tracked (Camera: 45-55 ajust the value depending on distance between tracking object and camera; Video: 35)
-RADIUS = 55
+RADIUS = 40
 
 # Set frame count number for tracking trails reset (when there is no hands being detected)
 FRAME = 100
 
+
 # Initialize frame_acount
 frame_count = 0
 
-# Get current time
-DATE= datetime.now().strftime('%Y:%m:%d')
 
 # Get video/camera input details
 """
@@ -62,21 +59,20 @@ fpsCamera  = int(num_frames / seconds)
 print ("Estimated frames per second : {0}".format(fpsCamera))    
 """
 
-# Define range of skin color in HSV
-#lower_thresh = np.array([0, 50, 0])
-#upper_thresh = np.array([120, 150, 255])
-
-# define range of skin color in HSV (works better with brown skin)
-lower_thresh = np.array([0, 48, 80], dtype = "uint8")
-upper_thresh = np.array([20, 255, 255], dtype = "uint8")
 
 # Create empty points array for hand trajectories tracking 
 points_left = []
 points_right = []
 
 # Initlaize K-Nearest Neighbors (KNN) background subtractor
-kernel_bgsub = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
-fgbg = cv2.createBackgroundSubtractorKNN()
+#kernel_bgsub = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+#fgbg = cv2.createBackgroundSubtractorKNN()
+
+# returns the elapsed milliseconds since the start of the program
+def milliseconds():
+   dt = datetime.now() - start_time
+   ms = (dt.days * 24 * 60 * 60 + dt.seconds) * 1000 + dt.microseconds / 1000.0
+   return ms
 
 # Sorting contour by area
 def get_contour_areas(contours):  
@@ -102,7 +98,7 @@ def plot_trajectories(center,str, clr):
     plt.ylabel('Y')
     plt.title(str + ' hand trajectories')
     plt.gca().invert_yaxis()  #Reverse Y-Axis in PyPlot (opencv choose the coordinate system of points/images from Top-Left corner)
-    plt.gca().invert_xaxis()  #Reverse X-Axis in PyPlot (Make trajectories like a Mirror View)
+    #plt.gca().invert_xaxis()  #Reverse X-Axis in PyPlot (Make trajectories like a Mirror View)
     plt.show()
     return None
 
@@ -117,13 +113,13 @@ def plot_trajectories_vstime(center,str):
     plt.ylabel('X-Y')
     plt.title(str + ' hand trajectories')
     plt.gca().invert_yaxis()  #Reverse Y-Axis in PyPlot (y reverted for:opencv choose the coordinate system of points/images from Top-Left corner; x reverted for: mirror effect)
-    plt.gca().invert_xaxis()
+    #plt.gca().invert_xaxis()  #Reverse X-Axis in PyPlot (Make trajectories like a Mirror View)
     plt.legend(loc='upper right')
     plt.show()
     return None
 
-
-#Plot 3D trjectories 
+#Plot 3D trjectories with Timeline on marker
+"""
 def plot_trajectories_3d(center, str, clr):
     xs = [x[0] for x in center]
     ys = [x[1] for x in center]
@@ -133,16 +129,78 @@ def plot_trajectories_3d(center, str, clr):
     #ax.plot3D(xs, ys, dates.date2num(ts), 'gray')
     ax.plot3D(xs, np.arange(0, len(xs)), ys, color= clr, marker ='o') 
     ax.set_xlabel('X')
-    ax.set_ylabel('Time')
+    ax.set_ylabel('Time (H:M:S)')
     ax.set_zlabel('Y')
     ax.set_title(str + '-Trajectory')
     plt.gca().invert_zaxis()  #Reverse Z-Axis in PyPlot (to revert y)
-    # place text annotations for timestamp  on a 3D plot
+    #plt.gca().invert_xaxis()  #Reverse X-Axis in PyPlot (Make trajectories like a Mirror View)
+    # place text annotations for timestamp  on a 3D plot ([ts] in hour:min:sec formate)
     for x, y, z, label in zip(xs, np.arange(0, len(xs)), ys, ts):
         ax.text(x, y, z, label)
    
     plt.show()
     return None
+"""
+
+#Plot 3D trjectories with Timeline in Y
+def plot_trajectories_3d(center, str, clr):
+    xs = [x[0] for x in center]
+    ys = [x[1] for x in center]
+    ts = [x[2] for x in center]  
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')    
+    
+    ax.plot3D(xs, ts, ys, color= clr, marker ='o') 
+    #ax.set_yticks =(0, -1, 100)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Time (ms)')
+    ax.set_zlabel('Y')
+    ax.set_title(str + '-Trajectory')
+    plt.gca().invert_zaxis()  #Reverse Z-Axis in PyPlot (to revert y)
+    #plt.gca().invert_xaxis()  #Reverse X-Axis in PyPlot (Make trajectories like a Mirror View)
+    plt.show()
+    return None
+ 
+# define the different colour convertion function blocks: from RBG/BGR to HSV/YCrCb/LAB/XYZ 
+def HSV():
+    con_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    cv2.imshow('HSV Colour Model Image:', con_img)
+    return con_img
+
+
+
+# map the inputs to the different colour convertion function blocks
+options = {1 : HSV,
+           
+}
+
+# define the different colour convertion threshold
+def HSV_thre():
+    # Selected Value sets
+    low_thresh = np.array([0, 48, 80], dtype = "uint8")
+    up_thresh = np.array([20, 255, 255], dtype = "uint8")
+    
+    #low_thresh = np.array([0, 10, 60], dtype = "uint8")
+    #up_thresh = np.array([20, 150, 255], dtype = "uint8")
+    
+    #DeepGaze Threshold (too much yellow)
+    #low_thresh = np.array([0, 58, 50], dtype = "uint8")
+    #up_thresh = np.array([30, 250, 255], dtype = "uint8")
+    return low_thresh, up_thresh
+
+
+
+# map the inputs to different colour convertion threshold
+options_thre = {1 : HSV_thre,
+               
+}
+
+#Set lower_thresh, upper_thresh for different colour convertion models
+lower_thresh, upper_thresh = options_thre[COLOUR_MODEL]()
+
+# Get current date & time
+DATE= datetime.now().strftime('%Y:%m:%d')
+start_time = datetime.now()
 
 # Loop video capture until break statement is exectured
 while cap.isOpened(): 
@@ -156,19 +214,25 @@ while cap.isOpened():
         # Get default camera/video window size
         Height, Width = frame.shape[:2]
        
-        # Convert image from RBG/BGR to HSV 
-        hsv_img = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        #Different colour convertion function blocks is invoked:
+        converted_img = options[COLOUR_MODEL]()
+       
         
         # Face Detection Using HAAR CASCADE 
         hc_face = cv2.CascadeClassifier("C:/Users/liangx/source/repos/Skin Detection/haarcascade_frontalface_alt/haarcascade_frontalface_alt.xml")
-        faces = hc_face.detectMultiScale(hsv_img)
+        faces = hc_face.detectMultiScale(converted_img)
         for (x,y,w,h) in faces:
-            cv2.rectangle(hsv_img, (x,y), (x+w,y+h), 255, thickness=2)
+            # If we do not draw a box on face, then use the code below 
+            #cv2.rectangle(converted_img, (x,y), (x+w,y+h), 255, thickness=2)
+
+            # If we draw a box on face to avoid face skin detection, then use the code below 
+            cv2.rectangle(converted_img, (x,y-30), (x+w+10, y+h+50), (255,255,255), -1)
             crop_img = frame[y+2:y+w, x+2:x+h]
             cv2.imshow('Face Detection', crop_img)
+            
        
         # Use inRange to capture only the values between lower & upper_thresh for skin detection
-        mask = cv2.inRange(hsv_img, lower_thresh, upper_thresh) 
+        mask = cv2.inRange(converted_img, lower_thresh, upper_thresh) 
         
         # Adding morphology effects to denoise
         kernel_morphology =np.ones((5, 5), np.uint8)
@@ -183,33 +247,24 @@ while cap.isOpened():
         rest1 = cv2.bitwise_and(frame, frame, mask= mask)
         
         # Apply KKN background subtraction to refine skin filtering result, i.e. to further remove static skin coulor related background (face will be fading out, if it does not move) 
-        fgmask = fgbg.apply(rest1)
-        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel_bgsub)
-        cv2.imshow('Background subtraction + Skin colour + Morpho Mask',fgmask) 
+        #fgmask = fgbg.apply(rest1)
+        #fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel_bgsub)
+        #cv2.imshow('Background subtraction + Skin colour + Morpho Mask',fgmask) 
 
         # Perform Bitwise AND on fgmask and rest1 frame
         # rest2 is results after applying background subtraction + morphology effects  + skin filtering
-        rest2 = cv2.bitwise_and(rest1, rest1, mask= fgmask)
+        #rest2 = cv2.bitwise_and(rest1, rest1, mask= fgmask)
 
-        # Find contours 
+        # Find contours on fgmask
         # cv2.RETR_EXTERNAL finds external contours only; cv2.CHAIN_APPROX_SIMPLE only provides start and end points of bounding contours, thus resulting in much more efficent storage of contour information.
-        _, contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        #_, contours, _ = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Find contours on mask
+        # cv2.RETR_EXTERNAL finds external contours only; cv2.CHAIN_APPROX_SIMPLE only provides start and end points of bounding contours, thus resulting in much more efficent storage of contour information.
+        _, contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         print ("Number of contours1 found = ", len(contours))
         #print(type(contours))   #The variable 'contours' are stored as a numpy array of (x,y) points that form the contour
-        
-        # Find Canny Edges (not helpful!)
-        """
-        edged = cv2.Canny(rest2, 30, 240)
-        cv2.imshow('Canny Edges', edged)
-        
-        # Find contours 
-        # cv2.RETR_EXTERNAL finds external contours only
-        # cv2.CHAIN_APPROX_SIMPLE only provides start and end points of bounding contours, thus resulting in much more efficent storage of contour information.
-        _, contours, _ = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        print ("Number of contours2 found = ", len(contours))
-        #print(type(contours))   #The variable 'contours' are stored as a numpy array of (x,y) points that form the contour 
-     
-        """
+            
         
         # Draw all Contours found
         #cv2.drawContours(rest2, contours, -1, (0,255,0), 3)
@@ -231,17 +286,17 @@ while cap.isOpened():
                 
                 # Draw Convex Hull Contour   
                 hull=cv2.convexHull(c)
-                cv2.drawContours(rest2, [hull], -1, (0,0,255), 3)
+                cv2.drawContours(rest1, [hull], -1, (0,0,255), 3)
                       
                 # Draw Normal Contour
-                cv2.drawContours(rest2, [c], -1, (255,0,0), 3)
+                cv2.drawContours(rest1, [c], -1, (255,0,0), 3)
 
                 # Bounding Box for CAMshift
                 #x,y,w,h = cv2.boundingRect(c)
                 #cv2.rectangle(rest2,(x,y),(x+w,y+h),(0,255,255),2)    
                          
                 # Show hands Contour
-                cv2.imshow('Contours by area', rest2)
+                cv2.imshow('Contours by area', rest1)
 
 
                 # Tracking Left hand   
@@ -249,7 +304,13 @@ while cap.isOpened():
                     (x, y), radius = cv2.minEnclosingCircle(c)
                     M = cv2.moments(c)
                     #center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]), datetime.now().strftime('%H:%M:%S'))
+
+                    #3D Plot in (Hour: Minute: Second) Format
+                    #center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]), datetime.now().strftime('%H:%M:%S'))
+                    
+                    #3D Plot in (mili second) Format
+                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]), milliseconds())
+                    
                     # Draw cirlce and leave the last center creating a trail
                     cv2.circle(frame, (int(x), int(y)), int(radius),(0, 0, 255), 2)
 
@@ -286,7 +347,8 @@ while cap.isOpened():
                     (x, y), radius = cv2.minEnclosingCircle(c)
                     M = cv2.moments(c)
                     #center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]),datetime.now().strftime('%H:%M:%S'))
+                    #center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]),datetime.now().strftime('%H:%M:%S'))
+                    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]),  milliseconds())
                     # Draw cirlce and leave the last center creating a trail
                     cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 0), 2)
                    
@@ -320,7 +382,7 @@ while cap.isOpened():
 
 
         # Display our object tracker
-        frame = cv2.flip(frame, 1)
+        #frame = cv2.flip(frame, 1)
         cv2.imshow("Object Tracker", frame)
        
 
